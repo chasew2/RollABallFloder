f7cb6e16384e0e2ea5f13e77243fd151cfd74ac5
Chase <chasew13579@gmail.com>
2025-01-17 14:52:13 -0800

    Error fixed in code movement works

--- RollABall/Assets/ball.cs
+++ RollABall/Assets/ball.cs
@@ -15,8 +15,31 @@ public class ball : MonoBehaviour
 void Update()
 
  {
+    Vector2 inputVector = Vector2.zero; // intialize our input vector
+ if (Input.GetKey(KeyCode.W))
+{
+    inputVector += Vector2.up; // "a += b" <=> "a = a + b"
+ }
+
+ if (Input.GetKey(KeyCode.S))
+ {
+    inputVector += Vector2.down;
+
+ }
+
+if (Input.GetKey(KeyCode.D))
+ {
+    inputVector += Vector2.right;
+
+ }
+
+ if (Input.GetKey(KeyCode.A))
+ {
+    inputVector += Vector2.left;
+
+ }
+ Debug.Log("Resultant Vector: " + inputVector);
     Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
     sphereRigidbody.AddForce(inputXZPlane);
-
  }
  }
